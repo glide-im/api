92cd988b7a43b65269b230802dfd9a0c82df8c58
chenf <chenf@surest.cn>
2022-07-08 15:56:55 +0800

    新增自动部署脚本

--- run.sh
+++ run.sh
@@ -1,12 +1,3 @@
-if [ -f api_linux_amd64.tar.gz ]
-then
-      tar -xzvf api_linux_amd64.tar.gz
-fi
-
-git pull
-git reset --hard origin/master
-go build cmd/api/main.go
-
 chmod +x api_linux
 pkill api_linux
 nohup ./api_linux >> log.output &
